c5efdd62ed5419328b0253d0b8a0be9b89295151
olivercolas <olivercolas.oc@gmail.com>
2020-06-27 20:29:51 +0300

    feat: added model for state and team result in store utils

--- src/models/team.ts
+++ src/models/team.ts
@@ -1,7 +1,20 @@
 export interface Team {
   name: string;
-  score: number;
+  points: number;
   gamesPlayed: number;
+  wins: number;
+  losses: number;
+  goalsScored: number;
+}
+
+export interface TeamState {
+  [key: string]: Team;
+}
+
+export interface TeamResult {
+  id: string;
+  points: number;
+  goalsScored: number;
   wins: number;
   losses: number;
 }
