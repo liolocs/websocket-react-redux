0b8f0890cb9d6c75e37491eca58ebc4cac03a085
olivercolas <olivercolas.oc@gmail.com>
2020-06-27 20:26:13 +0300

    chore: linting

--- src/api/games.ts
+++ src/api/games.ts
@@ -2,13 +2,11 @@ import axios from 'axios';
 import config from '../config';
 
 const axiosInstance = axios.create({
-    baseURL: config.BASE_URL
-})
+  baseURL: config.BASE_URL,
+});
 
-export const getTeamById = (teamId: string) => {
-    return axiosInstance.get(`/teams/${teamId}`)
-}
+export const getTeamById = async (teamId: string) => await axiosInstance.get(`/teams/${teamId}`);
 
 export default {
-    getTeamById
-}
+  getTeamById,
+};
