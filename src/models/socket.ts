925a9239854dbec71c69839e2ec5e543a30bdba2
olivercolas <olivercolas.oc@gmail.com>
2020-06-27 20:28:51 +0300

    feat: added new models for connection and state

--- src/models/socket.ts
+++ src/models/socket.ts
@@ -4,3 +4,11 @@ export type SocketMessage = {
   result: string;
   teams: [string, string];
 };
+
+export interface SocketConnection {
+  host: string;
+}
+
+export interface SocketState {
+	connections: SocketConnection[];
+}
